ca7ffa18c41de0c5a6e6939da463ae5e606a3cd7
Akshay Lokur <alokur@apple.com>
2019-12-13 15:36:20 +0800

    Changing logic to make it easier

--- src/com/lokur/PrintFibonacciSeries.java
+++ src/com/lokur/PrintFibonacciSeries.java
@@ -3,23 +3,25 @@ package com.lokur;
 public class PrintFibonacciSeries {
 
     // 0 1 1 2 3 5 8 13
-    static int fib(int n) {
-        if (n <= 0) {
-            return 0;
-        } else if (n == 1 || n == 2) {
-            return 1;
-        } else {
-            return fib(n - 2) + fib(n - 1);
+    static void fib(int p1, int p2, int printFibSeriesTill) {
+        if (p2 > printFibSeriesTill) {
+            return;
         }
+
+        int p3 = p2 + p1;
+        System.out.print(p3 + " ");
+        p1 = p2;
+        p2 = p3;
+
+        fib(p1, p2, printFibSeriesTill);
     }
 
 
     public static void main(String[] args) {
         int printFibSeriesTill = 5;
+        System.out.print("0 1 ");
 
-        for (int i = 0; i < printFibSeriesTill; i++) {
-            System.out.print(" " + fib(i));
-        }
+        fib(0, 1, printFibSeriesTill - 2); // -2 because first two are already printed
     }
 
 }
